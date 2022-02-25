0d9665e92c3dfc60434116c1b06f13c40a3049e2
Kushal Sharma <skushal746@gmail.com>
2022-02-25 02:53:44 -0500

    first commit

---
+++ src/main/java/com/example/userservice/springsecurity/jwt/UserServiceSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.userservice.springsecurity.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceSpringSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/userservice/springsecurity/jwt/UserServiceSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.userservice.springsecurity.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
